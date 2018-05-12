e87a4da336157eb5545dfd2cd3b0662e60d13f5c
Walter <relw1wa-github@yahoo.de>
2018-05-12 17:59:03 +0200

    Fix global-styles import

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-import '../../global-styles.scss';
+import '../../styles/global-styles.scss';
 import '../../config/font-awesome';
 
 import RecipeBox from './components/recipe-box';
